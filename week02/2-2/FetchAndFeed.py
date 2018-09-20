ba04b10e74720b62174448ab0033d873b46593ab
EVWang <810892226@qq.com>
2018-09-21 01:32:10 +0800

    week02

--- week02/2-2/FetchAndFeed.py
+++ week02/2-2/FetchAndFeed.py
@@ -19,7 +19,6 @@ def feed():
     input1 = tf.placeholder(tf.float32)
     input2 = tf.placeholder(tf.float32)
     output = tf.multiply(input1,input2)
-
     sess = tf.Session()
     print(sess.run(output,feed_dict={input1:[8.0],input2:[2.0]}))
 
